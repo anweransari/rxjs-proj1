172ea89bcccb482ecca5650dc85b53a31e611e5e
anweransari <anweransari99@gmail.com>
2022-01-21 17:10:41 -0500

    rxjs initial changes

--- index.ts
+++ index.ts
@@ -2,8 +2,12 @@ import './style.css';
 
 import { of, map, Observable } from 'rxjs';
 
-of('World')
-  .pipe(map((name) => `Hello, ${name}!`))
+of('World','Mom')
+  .pipe(map((name) => 
+  {
+   return `Hello, ${name}!`
+  })
+  )
   .subscribe(console.log);
 
 // Open the console in the bottom right to see results.
